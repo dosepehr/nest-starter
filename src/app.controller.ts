cb0c2fd422e563c5ba1160f9b518d6fb82bcbbbb
dosepehr <dosepehr@gmail.com>
2025-03-25 09:01:10 +0330

    add authGuard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, UseInterceptors, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LoggerInterceptor } from './utils/interceptors/Logger.interceptor';
+import { AuthGuard } from './utils/guards/auth.guard';
 
 @Controller()
 @UseInterceptors(LoggerInterceptor)
+@UseGuards(AuthGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
---
+++ src/utils/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization;
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
