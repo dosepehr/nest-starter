a1d28263d99de4a1a1bad212d9cb4cd6edc47bf9
dosepehr <dosepehr@gmail.com>
2025-08-04 11:29:18 +0330

    fix appController test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import type { Request } from 'express';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+        appController = app.get<AppController>(AppController);
     });
-  });
+
+    describe('root', () => {
+        it('should return "hi"', () => {
+            const mockRequest = {} as Request;
+            expect(appController.getHello(mockRequest)).toEqual({
+                message: 'hi',
+            });
+        });
+    });
 });
