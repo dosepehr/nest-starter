473bc5d4ad1fcc60eb34e2325c56d7106b5f5339
dosepehr <dosepehr@gmail.com>
2025-08-09 15:22:33 +0330

    add success response

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -4,6 +4,7 @@ import { UpdateCatDto } from './dto/update-cat.dto';
 import { Cat } from './entities/cat.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { SuccessResponse } from 'src/utils/interface/success-response.interface';
 
 @Injectable()
 export class CatService {
@@ -11,9 +12,13 @@ export class CatService {
         @InjectRepository(Cat)
         private readonly catRepository: Repository<Cat>,
     ) {}
-    async create(createCatDto: CreateCatDto) {
+    async create(createCatDto: CreateCatDto): Promise<SuccessResponse> {
         const cat = this.catRepository.create(createCatDto);
-        return await this.catRepository.save(cat);
+        await this.catRepository.save(cat);
+        return {
+            message: 'Cat created successfully',
+            status: true,
+        };
     }
 
     findAll() {
---
+++ src/utils/interface/success-response.interface.ts
@@ -0,0 +1,5 @@
+export interface SuccessResponse<T = undefined> {
+    message: string;
+    status: boolean;
+    data?: T;
+}
