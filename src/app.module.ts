4a6188e3a944fa3296e149ebbd9b48d0b40ad20f
dosepehr <dosepehr@gmail.com>
2025-08-09 10:36:18 +0330

    add new cat resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { CatModule } from './cat/cat.module';
 
 @Module({
-    imports: [],
+    imports: [CatModule],
     controllers: [AppController],
     providers: [AppService],
 })
