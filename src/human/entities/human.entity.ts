36a99c2a1655028d31f0fa354a0fb46661ebb4c8
dosepehr <dosepehr@gmail.com>
2025-08-09 16:03:42 +0330

    add human entity

--- src/human/entities/human.entity.ts
+++ src/human/entities/human.entity.ts
@@ -1,1 +1,37 @@
-export class Human {}
+import { Gender } from 'src/utils/enums/cat.enum';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Human {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    age: number;
+
+    @Column({
+        type: 'enum',
+        enum: Gender,
+        default: Gender.MALE,
+    })
+    gender: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+
+    @DeleteDateColumn({ type: 'timestamp', nullable: true })
+    deletedAt?: Date;
+}
